eb9aef7e8c8e5256377564ac9a2a4b68c7510e7f
jkal <ikalant@gmail.com>
2011-04-27 20:41:33 +0300

    Modify script to the new json format.

--- loaddata.py
+++ loaddata.py
@@ -4,5 +4,5 @@ import sys, redis, json
 r = redis.Redis()
 
 with open(sys.argv[1]) as f:
-    for i, session in enumerate(json.loads(f.read())):
-        r.hset('fosscomm2011:sessions:all', i, json.dumps(session))
+    for session in json.loads(f.read()):
+        r.hset('fosscomm2011:sessions:all', session['id'], json.dumps(session))
