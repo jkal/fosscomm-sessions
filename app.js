9aa5d7fbaf028bec21fa3c6b7eb617b14a20f529
jkal <ikalant@gmail.com>
2011-04-29 13:42:36 +0300

    Use flow-js to manage callback nesting.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var http = require('http'),
     io = require('socket.io'),
     redis = require('redis'),
-    xmpp = require('node-xmpp');
+    xmpp = require('node-xmpp'),
+    flow = require('flow');
 
 server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -29,9 +30,7 @@ cl.on('error', function(e) {
 });
 
 cl.on('online', function() {
-
     socket.on('connection', function(client) {
-
         client.on('message', function (data) {
             var message = JSON.parse(data);
 
@@ -41,30 +40,45 @@ cl.on('online', function() {
             };
 
             if (message.event == 'add') {
-                db.hget('fosscomm2011:sessions:all', message.id, function(err, data) {
-                    db.hexists('fosscomm2011:sessions:current', message.id, function (err, reply) {
-                        if (!reply) {
-                            db.hsetnx('fosscomm2011:sessions:current', message.id, data);
-                            var reply = {
-                                event: 'add',
-                                id: message.id,
-                                session: JSON.parse(data)
-                            };
-                            socket.broadcast(JSON.stringify(reply));
-                        };
-
-                        var xmpp_msg = JSON.parse(data).title + ' @ ' + JSON.parse(data).room;
-                        db.smembers('fosscomm2011:session:' + message.id, function(err, jids) {
-                            jids.forEach(function (jid, i) {
-                                console.log('node-xmpp: sending to ' + jid);
-                                cl.send(new xmpp.Element('message', { to: jid, type: 'chat'}).c('body').t(xmpp_msg));
-                            });
-                        });
-
-                    });
-                });
+                process_add(message.id);
             };
         });
     });
-
 });
+
+var process_add = flow.define(
+
+    function(msg_id) {
+        db.hget('fosscomm2011:sessions:all', msg_id, this);
+        this.msg_id = msg_id;
+    },
+
+    function(err, data) {
+        db.hexists('fosscomm2011:sessions:current', this.msg_id, this);
+        this.data = data;
+    },
+
+    function(err, reply) {
+        if (!reply) {
+            db.hsetnx('fosscomm2011:sessions:current', this.msg_id, this.data);
+            var reply = {
+                event: 'add',
+                id: this.msg_id,
+                session: JSON.parse(this.data)
+            };
+            socket.broadcast(JSON.stringify(reply));
+
+            this.xmpp_msg = JSON.parse(this.data).title + ' @ ' + JSON.parse(this.data).room;
+
+            db.smembers('fosscomm2011:session:' + this.msg_id, this);
+        }
+    },
+
+    function(err, jids) {
+        var that = this;
+        jids.forEach(function (jid, i) {
+            console.log('node-xmpp: sending to ' + jid);
+            cl.send(new xmpp.Element('message', { to: jid, type: 'chat'}).c('body').t(that.xmpp_msg));
+        });
+    }
+);
